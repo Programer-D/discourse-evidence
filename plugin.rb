5728df31939dde78af035d64a4a5aec605410ab2
Programer-D <dross.program@gmail.com>
2021-05-10 04:24:48 +0900

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 # name: discourse-evidence
 # version: 0.7.0
 gem 'aws-sdk-dynamodb', '1.60.0'
-gem 'aws-sdk-s3', '1.94.1'
+gem 'aws-sdk-s3', '1.94.0'
 enabled_site_setting :evidence_uploader_enabled
 
 after_initialize do
